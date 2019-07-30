7b72d2c02d3991c20d40c6511b21158a3db75a0e
canab <canab.ua@gmail.com>
2019-07-30 17:47:52 +0300

    impl automated testing & calibration

--- tools/ui.py
+++ tools/ui.py
@@ -1,5 +1,19 @@
-def main():
+def draw_menu():
+    print("\n")
+    print("Select:")
+    print("---------------------")
+    print("(f) Flush firmware")
+    print("(c) Calibrate")
+    print("(t) Test")
 
-    pass
+
+def make_choise():
+    nb = input('Choose a number: ')
+    print(nb)
+
+
+def main():
+    draw_menu()
+    make_choise()
 
 main()
