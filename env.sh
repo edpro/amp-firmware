4cb358382915fb01b232ebd3653b35f90da42256
canab <canab.ua@gmail.com>
2019-08-06 12:27:37 +0300

    move all images to ./images

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
-PYTHON_PATH=$(realpath ../toolchain/python)
-export PATH=$PYTHON_PATH:$PATH
+PYTHON_DIR=$(realpath ../toolchain/python)
+export PATH=$PYTHON_DIR:$PATH
 export PYTHONDONTWRITEBYTECODE=1
+export PYTHONPATH=$PWD:$PYTHONPATH
 echo "$(which python)"
 echo "$(python --version)"
--- powersource.sh
+++ powersource.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")" || exit 1
 source ./env.sh
-
 ./check-updates.sh
-
 python tools/ps_main.py
 
 if [[ "$?" != "0" ]]; then
