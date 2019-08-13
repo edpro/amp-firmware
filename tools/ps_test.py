80767e8304eb04ba77ca52504b61642e22de63bb
canab <canab.ua@gmail.com>
2019-08-13 13:55:44 +0300

    ps_test

--- tools/ps_test.py
+++ tools/ps_test.py
@@ -22,6 +22,8 @@ VAC_ZERO_ABS = 0.03
 ADC_REL = 0.02
 ADC_ZERO_ABS = 0.01
 
+AAC_ABS = 0.01
+
 FREQ_REL = 0.01
 
 
@@ -76,7 +78,8 @@ def test_v_dc(ps: EdproPS, ri: RigolDevice) -> bool:
         ri_val = ri.measure_vdc()
         ea = eabs(ps_val, ri_val)
         er = erel(ps_val, ri_val)
-        rec.trace(f"step: {step_u:0.1f}V | ps: {ps_val:0.6f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
+        rec.trace(
+            f"step: {step_u:0.1f}V | ps: {ps_val:0.6f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
         if level == 0:
             rec.expect_abs(ps_val, ri_val, VDC_ZERO_ABS)
         else:
@@ -105,7 +108,8 @@ def test_v_ac(ps: EdproPS, ri: RigolDevice) -> bool:
         ri_val = ri.measure_vac()
         ea = eabs(ps_val, ri_val)
         er = erel(ps_val, ri_val)
-        t.trace(f"step: {step_u:0.1f}V | ps: {ps_val:0.6f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
+        t.trace(
+            f"step: {step_u:0.1f}V | ps: {ps_val:0.6f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
         if level == 0:
             t.expect_abs(ps_val, ri_val, VAC_ZERO_ABS)
         else:
@@ -181,7 +185,8 @@ def test_a_dc(ps: EdproPS, ri: RigolDevice) -> bool:
         ri_val = ri.measure_vdc()
         ea = eabs(ps_val.I, ri_val)
         er = erel(ps_val.I, ri_val)
-        t.trace(f"step: {step_u:0.1f}V | U: {ps_val.U:0.3f} I: {ps_val.I:0.3f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
+        t.trace(
+            f"step: {step_u:0.1f}V | U: {ps_val.U:0.3f} I: {ps_val.I:0.3f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
         t.expect_abs(step_u, ps_val.U, VDC_STEP_ABS)
         if level == 0:
             t.expect_abs(0, ri_val, 0.01)
@@ -189,6 +194,33 @@ def test_a_dc(ps: EdproPS, ri: RigolDevice) -> bool:
         else:
             t.expect_rel(ps_val.U / CIRCUIT_R, ri_val, 0.05)
             t.expect_rel(ri_val, ps_val.I, ADC_REL)
+
+    t.print_result()
+    return t.success
+
+
+def test_a_ac(ps: EdproPS, ri: RigolDevice) -> bool:
+    t = TestReporter("tast_aac")
+    ri.mode(RigolMode.VAC_20)
+    ps.cmd("mode ac")
+    ps.cmd("set l 0")
+    ps.cmd("set f 1000")
+    wait_mode()
+
+    levels = [0, 2, 4, 6, 10, 0]
+
+    for level in levels:
+        step_u = 0.1 * level
+        ps.cmd(f"set l {level}")
+        time.sleep(0.25)
+        ps_val = ps.request_values()
+        ri_val = ri.measure_vac()
+        ea = eabs(ps_val.I, ri_val)
+        er = erel(ps_val.I, ri_val)
+        t.trace(
+            f"step: {step_u:0.1f}V | U: {ps_val.U:0.3f} I: {ps_val.I:0.3f} | rigol: {ri_val:0.6f} | abs: {ea:0.6f} | rel: {er * 100:0.2f}%")
+        t.expect_abs(step_u, ps_val.U, VAC_STEP_ABS)
+        t.expect_abs(ri_val, ps_val.I, AAC_ABS)
 
     t.print_result()
     return t.success
@@ -220,6 +252,7 @@ def ps_run_test():
             while True:
                 try:
                     check(test_a_dc(ps, ri), "Test failed!")
+                    check(test_a_ac(ps, ri), "Test failed!")
                     break
                 except LoggedError:
                     choise = prompt("<Enter> - continue, <r> - retry: ")
@@ -244,7 +277,8 @@ def _run():
         # check(test_v_dc(ps, ri), "Test Failed!")
         # check(test_v_ac(ps, ri), "Test Failed!")
         # check(test_freq(ps, ri), "Test Failed!")
-        test_a_dc(ps, ri)
+        # test_a_dc(ps, ri)
+        test_a_ac(ps, ri)
 
     except LoggedError:
         pass
