f8ac5d1e721ae4585af32586106a3cf08cc2436b
canab <canab.ua@gmail.com>
2019-09-23 13:17:02 +0300

    menu

--- tools/ui/menu.py
+++ tools/ui/menu.py
@@ -1,57 +1,124 @@
+from typing import List, Callable
+
 import curses
 
 #  typings for curses in /stub are taken from:
 #  https://github.com/python/typeshed/blob/master/stdlib/2and3/_curses.pyi
 from tools.common.screen import init_win_console
 
+PAIR_DEFAULT = 1
+PAIR_SELECTED = 2
+
 scr = curses.initscr()
 curses.start_color()
+curses.init_pair(PAIR_DEFAULT, curses.COLOR_WHITE, curses.COLOR_BLACK)
+curses.init_pair(PAIR_SELECTED, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
 
-def init_curses():
-    curses.noecho()
-    curses.cbreak()
-    scr.keypad(True)
+class MenuDef:
+    items: List['MenuItem'] = []
+    index: int = 0
+
+    def __init__(self, items: List['MenuItem']):
+        self.items = items
+        self.index = 0
+
+    def move_prev(self):
+        self.index = (self.index - 1) % len(self.items)
+
+    def move_next(self):
+        self.index = (self.index + 1) % len(self.items)
 
 
-def dispose_curses():
-    curses.nocbreak()
-    scr.keypad(False)
-    curses.echo()
-    curses.endwin()
+class MenuItem:
+    title: str
+    action: Callable
+    submenu: MenuDef
+
+    def __init__(self, title: str, action: Callable = None, submenu: MenuDef = None):
+        self.title = title
+        self.action = action
+        self.submenu = submenu
 
 
-class Menu():
-    def __init__(self):
-        pass
+class MenuCol:
+    items: List[MenuItem]
+    index: int
 
-    def show(self):
-        init_curses()
+
+class UI:
+    title: str
+    main_menu: MenuDef
+
+    def init(self):
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        scr.keypad(True)
+
+    def dispose(self):
+        curses.nocbreak()
+        curses.curs_set(1)
+        scr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+    def draw_title(self):
+        x = 1
+        y = 1
+        scr.addstr(y, x, self.title, curses.color_pair(PAIR_DEFAULT))
+
+    def draw_menu(self):
+        x = 1
+        y = 3
+        scr.addstr(y, x, '|')
+        x += 1
+        for i, it in enumerate(self.main_menu.items):
+            if i == self.main_menu.index:
+                text_pair = PAIR_SELECTED
+            else:
+                text_pair = PAIR_DEFAULT
+            title = f' {self.main_menu.items[i].title} '
+            scr.addstr(y, x, title, curses.color_pair(text_pair))
+            x += len(title)
+            scr.addstr(y, x, '|')
+            x += 1
+
+    def draw(self):
         scr.clear()
-        scr.addstr(0, 0, 'curses.A_REVERSE', curses.A_REVERSE)
-        scr.addstr(1, 0, 'curses.A_BOLD', curses.A_BOLD)
-        scr.addstr(2, 0, 'curses.A_STANDOUT', curses.A_STANDOUT)
-        scr.addstr(3, 0, 'curses.A_DIM', curses.A_DIM)
-        scr.addstr(4, 0, 'DEFAULT')
-        scr.addstr(5, 0, "Pretty text", curses.color_pair(1))
-        scr.addstr(6, 0, "Pretty text", curses.color_pair(2))
-
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
-        scr.addstr(7, 0, f"has_color: {curses.has_colors()}", curses.color_pair(1))
-        scr.addstr(7, 0, f"can_change_color: {curses.can_change_color()}")
+        self.draw_title()
+        self.draw_menu()
         scr.refresh()
 
-    def hide(self):
-        scr.clear()
-        dispose_curses()
+    def read(self) -> bool:
+        ch = scr.getkey()
+        if ch == 'KEY_LEFT':
+            self.main_menu.move_prev()
+        elif ch == 'KEY_RIGHT':
+            self.main_menu.move_next()
+        elif ch == 'q':
+            return False
+        return True
+
+    def show_menu(self):
+        self.draw()
+        while self.read():
+            self.draw()
 
 
 def dev_run():
     init_win_console()
-    m = Menu()
-    m.show()
-    scr.getkey()
-    m.hide()
+    ui = UI()
+    ui.init()
+    ui.title = "DEVICE_NAME:"
+    ui.main_menu = MenuDef([
+        MenuItem("Flash"),
+        MenuItem("Test"),
+        MenuItem("Calibration"),
+        MenuItem("Quit"),
+    ])
+    ui.show_menu()
+    ui.dispose()
 
 
 if __name__ == "__main__":
