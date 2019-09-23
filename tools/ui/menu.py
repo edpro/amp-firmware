2f402cbf24811d67b4c6c1a455cd7122693ad815
canab <canab.ua@gmail.com>
2019-09-23 15:46:22 +0300

    menu

--- tools/ui/menu.py
+++ tools/ui/menu.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import List, Callable, Optional
 
 import curses
 
@@ -37,6 +37,8 @@ class MenuItem:
     title: str
     action: Callable
     submenu: MenuDef
+    x: int = 0
+    y: int = 0
 
     def __init__(self, title: str, action: Callable = None, submenu: MenuDef = None):
         self.title = title
@@ -50,8 +52,10 @@ class MenuCol:
 
 
 class UI:
-    title: str
-    main_menu: MenuDef
+    title: str = ""
+    main_menu: MenuDef = None
+    submenu: Optional[MenuDef] = None
+    submenu_shown: bool = False
 
     _quit_requested: bool = False
 
@@ -79,35 +83,70 @@ class UI:
         scr.addstr(y, x, '|')
         x += 1
         for i, it in enumerate(self.main_menu.items):
-            if i == self.main_menu.index:
-                text_pair = PAIR_SELECTED
-            else:
-                text_pair = PAIR_DEFAULT
             title = f' {self.main_menu.items[i].title} '
-            scr.addstr(y, x, title, curses.color_pair(text_pair))
+            color_num = PAIR_SELECTED if i == self.main_menu.index else PAIR_DEFAULT
+            it.x = x
+            it.y = y
+            scr.addstr(y, x, title, curses.color_pair(color_num))
             x += len(title)
             scr.addstr(y, x, '|')
             x += 1
+
+    def draw_submenu(self):
+        if self.submenu is None:
+            return
+        main_item = self.main_menu.current_item()
+        x = main_item.x
+        y = main_item.y + 1
+        for i, it in enumerate(self.submenu.items):
+            text_pair = PAIR_SELECTED if i == self.submenu.index else PAIR_DEFAULT
+            it.x = x
+            it.y = y
+            title = f' {self.submenu.items[i].title} '
+            scr.addstr(y, x, title, curses.color_pair(text_pair))
+            y += 1
+
 
     def draw(self):
         scr.clear()
         self.draw_title()
         self.draw_menu()
+        self.draw_submenu()
         scr.refresh()
+
+
+    def get_submenu(self) -> Optional[MenuDef]:
+        return self.main_menu.current_item().submenu
 
     def quit(self):
         self._quit_requested = True
 
     def read(self) -> bool:
         ch = scr.getch()
+        if ch == 27:
+            if self.submenu is None:
+                self.quit()
+            else:
+                self.submenu = None
+                self.submenu_shown = False
         if ch == curses.KEY_LEFT:
             self.main_menu.move_prev()
+            if self.submenu_shown:
+                self.submenu = self.main_menu.current_item().submenu
         elif ch == curses.KEY_RIGHT:
             self.main_menu.move_next()
+            if self.submenu_shown:
+                self.submenu = self.main_menu.current_item().submenu
         elif ch == curses.KEY_ENTER or ch == 10 or ch == 13:
             it = self.main_menu.current_item()
             if it.action:
                 it.action()
+            elif not self.submenu_shown:
+                self.submenu_shown = True
+                self.submenu = self.main_menu.current_item().submenu
+            elif self.submenu:
+                self.submenu.current_item().action()
+
         elif ch == ord('q'):
             self._quit_requested = True
 
@@ -125,9 +164,23 @@ def dev_run():
     ui.init()
     ui.title = "DEVICE_NAME:"
     ui.main_menu = MenuDef([
-        MenuItem("Firmware"),
-        MenuItem("Test"),
-        MenuItem("Calibration"),
+        MenuItem("Firmware", submenu=MenuDef([
+            MenuItem("Flash Firmware"),
+        ])),
+        MenuItem("Calibration", submenu=MenuDef([
+            MenuItem("Calibrate ALL"),
+            MenuItem("Calibrate VDC"),
+            MenuItem("Calibrate VAC"),
+            MenuItem("Calibrate ADC"),
+            MenuItem("Calibrate AAC"),
+        ])),
+        MenuItem("Test", submenu=MenuDef([
+            MenuItem("Test ALL"),
+            MenuItem("Test VDC"),
+            MenuItem("Test VAC"),
+            MenuItem("Test ADC"),
+            MenuItem("Test AAC"),
+        ])),
         MenuItem("Quit", action=ui.quit),
     ])
     ui.show_menu()
