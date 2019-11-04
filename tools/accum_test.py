ee09f4f62021c324cd2fbb8403fb433f8e79b121
canab <canab.ua@gmail.com>
2019-11-04 17:42:35 +0200

    accum test

--- tools/accum_test.py
+++ tools/accum_test.py
@@ -1,10 +1,12 @@
 # noinspection PyMethodParameters
+import time
 
 from tools.common.logger import LoggedError, Logger
 from tools.common.screen import scr_prompt
 from tools.devices.rigol_load import RigolLoad
 
-MIN_V = 4.5
+MIN_V = 3.5
+MAX_R = 0.2
 
 load = RigolLoad()
 logger = Logger("accum_test")
@@ -12,9 +14,24 @@ logger = Logger("accum_test")
 
 def test_internal_r():
     logger.info("Testing internal resiatance...")
-    v = load.measure_voltage()
-    if (v < MIN_V):
+
+    load.set_input(0)
+    time.sleep(0.5)
+    v_unload = load.measure_voltage()
+    if (v_unload < MIN_V):
         logger.throw(f"Voltage too low: V < {MIN_V}")
+
+    load.set_const_current(0.1)
+    load.set_input(1)
+    time.sleep(1)
+    v_load = load.measure_voltage()
+    i_load = load.measure_current()
+    load.set_input(0)
+
+    r = (v_unload - v_load) / i_load
+    logger.info(f"Internal_R: {r:0.3}")
+    if (r > MAX_R):
+        logger.throw(f"Intenal resistance it too high: R > {MAX_R}")
 
 
 def test_current_1():
