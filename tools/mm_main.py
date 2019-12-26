0c8525da3d6769c12aa009cc3483483ce8443df5
canab <canab.ua@gmail.com>
2019-12-26 12:25:59 +0200

    workflows

--- tools/mm_main.py
+++ tools/mm_main.py
@@ -20,10 +20,28 @@ def firmware_update():
     flash_firmware("./images/multimeter", UartStr.CP210)
 
 
-def test_voltage_r():
+def cal_volt_r():
+    MMCalibration(MMCalFlags.DC0
+                  | MMCalFlags.VDC
+                  | MMCalFlags.AC0
+                  | MMCalFlags.VAC
+                  | MMCalFlags.R).run()
+
+
+def test_volt_r():
     MMTestVDC().run()
     MMTestVAC(run_fast=True).run()
     MMTestR().run()
+
+
+def cal_test_vr():
+    if not cal_volt_r(): return
+    if not test_volt_r(): return
+
+
+def cal_current():
+    MMCalibration(MMCalFlags.ADC
+                  | MMCalFlags.AAC).run()
 
 
 def test_current():
@@ -31,23 +49,40 @@ def test_current():
     MMTestAAC(run_fast=True).run()
 
 
+def cal_test_c():
+    if not cal_volt_r(): return
+    if not test_volt_r(): return
+
+
 ps_menu = MenuDef([
     MenuItem("Device", submenu=MenuDef([
         MenuItem("Firmware Init", lambda: firmware_init()),
         MenuItem("Firmware Update", lambda: firmware_update()),
+        MenuItem("-"),
+        MenuItem("Calibrate & Test Volt/R", lambda: cal_test_vr()),
+        MenuItem("Calibrate & Test Current", lambda: cal_test_c()),
+        MenuItem("-"),
         MenuItem("Connect", lambda: EdproMM().show_log(), is_pause=False),
         MenuItem("Info", lambda: print_esp_info(UartStr.CP210)),
 
     ])),
-    MenuItem("Calibration", submenu=MenuDef([
-        MenuItem("AC/DC voltage & R", lambda: MMCalibration(MMCalFlags.DC0
-                                                            | MMCalFlags.VDC
-                                                            | MMCalFlags.AC0
-                                                            | MMCalFlags.VAC
-                                                            | MMCalFlags.R).run()),
 
-        MenuItem("AC/DC current", lambda: MMCalibration(MMCalFlags.ADC
-                                                        | MMCalFlags.AAC).run()),
+    MenuItem("Flows", submenu=MenuDef([
+        MenuItem("Test AC/DC Voltage & R", lambda: test_volt_r()),
+        MenuItem("Test AC/DC Current", lambda: test_current()),
+        MenuItem("-"),
+        MenuItem("Test DC Voltage", lambda: MMTestVDC().run()),
+        MenuItem("Test DC Current", lambda: MMTestADC().run()),
+        MenuItem("-"),
+        MenuItem("Test AC Voltage", lambda: MMTestVAC().run()),
+        MenuItem("Test AC Current", lambda: MMTestAAC().run()),
+        MenuItem("-"),
+        MenuItem("Test R", lambda: MMTestR().run()),
+    ])),
+
+    MenuItem("Calibration", submenu=MenuDef([
+        MenuItem("AC/DC voltage & R", lambda: cal_volt_r()),
+        MenuItem("AC/DC current", lambda: cal_current()),
         MenuItem("-"),
         MenuItem("DC zero", lambda: MMCalibration(MMCalFlags.DC0).run()),
         MenuItem("DC voltage", lambda: MMCalibration(MMCalFlags.VDC).run()),
@@ -61,7 +96,7 @@ ps_menu = MenuDef([
 
     ])),
     MenuItem("Test", submenu=MenuDef([
-        MenuItem("Test AC/DC Voltage & R", lambda: test_voltage_r()),
+        MenuItem("Test AC/DC Voltage & R", lambda: test_volt_r()),
         MenuItem("Test AC/DC Current", lambda: test_current()),
         MenuItem("-"),
         MenuItem("Test DC Voltage", lambda: MMTestVDC().run()),
